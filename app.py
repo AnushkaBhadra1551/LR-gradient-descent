2b89c3719e92622fab15b91106c7d63fb7a1cca3
AnushkaBhadra1551 <anushka15official@gmail.com>
2020-11-28 21:52:53 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from sklearn import datasets
+from gradient_descent import LinearRegressionGD
+
+X,y = datasets.load_diabetes(return_X_y=True)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=10)
+
+reg = LinearRegressionGD(epochs=10000,learning_rate=0.1)
+
+reg.fit(X_train,y_train)
+
+y_pred = reg.predict(X_test)
+
+from sklearn.metrics import r2_score
+print(r2_score(y_test,y_pred))
